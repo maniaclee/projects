a2b7014ae2ac9454e51402d84bb6db8cf93a46f2
lipeng <lipeng@jk.cn>
2015-07-24 01:08:58 +0800

    scala main

---
+++ src/main/scala/scala/basic/Starter.scala
@@ -0,0 +1,14 @@
+package scala.basic
+
+/**
+ * Created by lipeng on 15/7/24.
+ */
+object Starter__Main {
+  def main(args: Array[String]) {
+    println("sdf")
+  }
+}
+
+object Starter__App extends App {
+  println("start from app")
+}
