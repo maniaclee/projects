63c1e8c6f42531542a1402ca95ea898fe31bdb38
lipeng <lipeng@jk.cn>
2015-08-06 00:15:37 +0800

    scala and lambda

--- src/main/scala/psyco/scala/basic/Implicit.scala
+++ src/main/scala/psyco/scala/basic/Implicit.scala
@@ -1,23 +1,56 @@
-package psyco.scala.basic
-
 /**
  * Created by lipeng on 15/8/5.
  */
-object AppleUtils {
+
+package psyco.test {
+package implicits {
+
+
+object ImplicitMethod extends App {
+
+  class A(name: String) {
+    override def toString: String = s"A-----($name)"
+  }
+
+  class RichA(a: A) {
+    def rich = print(a)
+  }
+
+  implicit def int2string(a: A) = new RichA(a)
+
+  val a = new A("Atest")
+  a.rich
+}
+
+object ImplicitParameter extends App {
+  /**
+   * implicit parameter按类型只能指定一个
+   * 在没有传参时自动补齐，类似default parameter value
+   * 同类型定义两个implicit parameter==> ambiguous implicit values
+   */
+  implicit val namexxxx: String = "implicit name"
+  //  implicit val name2: String = "implicit name2"
+
+  def echo(implicit name: String) = println(s"echo--->$name")
+
+  echo("fuck")
+  echo
+}
+
+object ImplicitClass extends App {
 
   implicit class get(s: String) {
     def getApple = new Apple(s)
   }
 
+  case class Apple(name: String)
+
+  println(this.getClass.getPackage.getName)
+  println("hello".getApple)
+
 }
 
-case class Apple(name: String)
+}
 
-
-object Implicit extends App {
-
-  import AppleUtils._
-
-  println("hello".getApple)
 
 }
