563362c90760967ecee2c06236b15c58a67d0e7a
lipeng <dianxiangan@aliyun.com>
2015-09-01 17:53:17 +0800

    aop 原理 & fix  logger stackoverflow

--- src/main/java/psyco/aop/Aspects.java
+++ src/main/java/psyco/aop/Aspects.java
@@ -11,6 +11,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 /**
+ * Aop代理分动态/静态代理两种
+ * 静态Aop为AspectJ,忽略掉(编译阶段增强)
+ * SpringAop属于动态代理AOP（内存中“临时”生成 AOP 动态代理类，因此也被称为运行时增强）
+ * Spring Aop = AspectJ的注解 + cglib/jdk
+ * 接口   ==>jdk
+ * 类    ==> cglib
+ * <p>
+ * <p>
  * Created by lipeng on 15/8/11.
  */
 @Aspect
@@ -28,8 +36,7 @@ public class Aspects {
 
     @Before("anyMethod() && args(name)")
     public void doAccessCheck(String name) {
-        System.out.println(name);
-        System.out.println("前置通知");
+        System.out.println("前置通知" + name);
     }
 
     @Around("anyMethod() ")
@@ -37,7 +44,10 @@ public class Aspects {
         try {
             Object[] args = pjp.getArgs();
             Signature sig = pjp.getSignature();
+            System.out.println("Aroud---------------");
             Object re = pjp.proceed(args);
+            /** 这里从类名可以看出spring用的到底是cglib/jdk 代理 */
+            System.out.println(pjp.getThis().getClass().getName());
             logger.debug("around=> para:{} , re:{},signature:{}", args, re, sig);
             logger.debug("type:{}", sig.getDeclaringType().getName());
             logger.debug("typeName:{}", sig.getDeclaringTypeName());
